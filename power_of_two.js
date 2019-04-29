b881aafbf5d918efc28d6f71193382dbdba986e0
Alvin Escobar <alvincsproductions@gmail.com>
2019-04-29 18:12:43 -0500

    Finished

---
+++ power_of_two.js
@@ -0,0 +1,13 @@
+var isPowerOfTwo = function(n) {
+    return Math.log2(n) % 1 === 0;
+};
+
+console.log(Math.log2(2));
+console.log(Math.log2(4));
+console.log(Math.log2(8));
+console.log(Math.log2(16));
+console.log(Math.log2(32));
+console.log(Math.log2(64));
+console.log(Math.log2(218));
+
+
